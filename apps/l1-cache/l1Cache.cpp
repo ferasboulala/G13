01a6529d9f6eb05ec5d951e0c043216fa89edc32
Feras <ferasboulala@gmail.com>
2023-02-24 15:45:41 -0500

    Use Satollo Algorithm

--- apps/l1-cache/l1Cache.cpp
+++ apps/l1-cache/l1Cache.cpp
@@ -18,15 +18,18 @@
 #include "measure.hh"
 
 template <typename T>
-std::vector<T> generateRandomIndices(uint64_t size) {
+std::vector<T> satolloRandomIndices(uint64_t size) {
+    assert(size >= 2);
     std::random_device               randomDevice;
     std::mt19937                     generator(randomDevice());
-    std::uniform_int_distribution<T> distribution(0, size - 1);
+    std::uniform_int_distribution<T> distribution;
     std::vector<T>                   indices(size);
 
-    for (uint64_t i = 0; i < size; ++i) {
-        indices[i] = distribution(generator);
+    std::iota(indices.begin(), indices.end(), 0);
+    for (uint64_t i = size - 1; i > 1; --i) {
+        std::swap(indices[i], indices[distribution(generator) % i]);
     }
+    std::swap(indices[0], indices[1]);
 
     return indices;
 }
@@ -55,16 +58,14 @@ int main(int argc, char **argv) {
     assert(commandQueue);
 
     static constexpr uint64_t maxSize = 1 << 22;
-    static constexpr uint64_t nReads = maxSize * 8;
-    for (uint64_t size = 1 << 10; size <= maxSize; size <<= 1) {
-        const uint64_t nIterations = nReads / size;
+    static constexpr uint32_t nExpectedReads = maxSize * 8;  // 128 MB
+    for (uint64_t size = 1 << 8; size <= maxSize; size <<= 1) {
+        const uint32_t nIterations = nExpectedReads / size;
 
-        auto indicesPermutationBuffer =
+        auto indicesBuffer =
             device->newBuffer(size * sizeof(uint32_t), MTL::ResourceStorageModeShared);
-        const auto indicesPermutation = generateRandomIndices<uint32_t>(size);
-        std::memcpy(indicesPermutationBuffer->contents(),
-                    indicesPermutation.data(),
-                    indicesPermutationBuffer->allocatedSize());
+        const auto indices = satolloRandomIndices<uint32_t>(size);
+        std::memcpy(indicesBuffer->contents(), indices.data(), indicesBuffer->allocatedSize());
 
         MTL::CommandBuffer *commandBuffer = commandQueue->commandBuffer();
         assert(commandBuffer);
@@ -72,13 +73,9 @@ int main(int argc, char **argv) {
         MTL::ComputeCommandEncoder *computeEncoder = commandBuffer->computeCommandEncoder();
         assert(computeEncoder);
 
-        auto inputBuffer = device->newBuffer(size, MTL::ResourceStorageModeShared);
-
         computeEncoder->setComputePipelineState(computePipelineState);
-        computeEncoder->setBuffer(inputBuffer, 0, 0);
-        computeEncoder->setBuffer(indicesPermutationBuffer, 0, 1);
-        computeEncoder->setBytes(&size, sizeof(size), 2);
-        computeEncoder->setBytes(&nIterations, sizeof(nIterations), 3);
+        computeEncoder->setBuffer(indicesBuffer, 0, 0);
+        computeEncoder->setBytes(&nIterations, sizeof(nIterations), 1);
 
         const MTL::Size threadgroupSize = MTL::Size::Make(1, 1, 1);
         const MTL::Size gridSize = MTL::Size::Make(1, 1, 1);
@@ -90,7 +87,11 @@ int main(int argc, char **argv) {
             commandBuffer->waitUntilCompleted();
         };
         const auto duration = g13::measureTime(commit);
-        printf("%12llu %12lld ns %4lld ns/iter\n", size, duration, duration / nReads);
+
+        printf("%12u %12lld ns %4lld ns/read\n",
+               uint32_t(indicesBuffer->allocatedSize()),
+               duration,
+               duration / nExpectedReads);
         fflush(stdout);
     }
 
--- apps/l1-cache/l1Cache.metal
+++ apps/l1-cache/l1Cache.metal
@@ -1,21 +1,19 @@
 #include <metal_stdlib>
 using namespace metal;
 
-#define REPEAT2(x) x x 
-#define REPEAT4(x) REPEAT2(x) REPEAT2(x)
-#define REPEAT8(x) REPEAT4(x) REPEAT4(x)
-#define REPEAT16(x) REPEAT8(x) REPEAT8(x)
-#define REPEAT(x) REPEAT16(x) REPEAT16(x)
-
 kernel void l1CacheIntrospection(
-    device char *inputBuffer,
-    device const uint32_t *indicesPermutation,
-    device const uint64_t &bufferSize,
-    device const uint64_t &nIterations)
+    device uint32_t *indices,
+    device const uint32_t &nIterations)
 {
-    for (unsigned iter = 0; iter < nIterations; ++iter) {
-        for (uint64_t i = 0; i < bufferSize; ++i) {
-            inputBuffer[indicesPermutation[i++]]++;
+    uint32_t iter = 0;
+    uint32_t dummy = 123;
+    do {
+        uint32_t ptr = indices[0];
+        while (ptr) {
+            ptr = indices[ptr];
         }
-    }
+        dummy += indices[ptr];
+    } while (++iter < nIterations);
+
+    indices[0] = dummy;
 }
